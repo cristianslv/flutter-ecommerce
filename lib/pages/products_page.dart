6cf2ed4e307bf99e343e3fbc18574e549c8ad0d8
cristianslv <cristianeduardo.dev@gmail.com>
2021-05-12 22:49:34 -0300

    Modifications in Products Page

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:app/models/app_state.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -21,13 +22,33 @@ class ProductsPageState extends State<ProductsPage> {
     widget.onInit();
   }
 
-  @override 
-  Widget build(BuildContext context) {
-    return StoreConnector<AppState, AppState>(
+  final _appBar = PreferredSize(
+    preferredSize: Size.fromHeight(60.0),
+    child: StoreConnector<AppState, AppState>(
       converter: (store) => store.state,
       builder: (context, state) {
-        return state.user != null ? Text(state.user.username) : Text("");
-      },
+        return AppBar(
+          centerTitle: true,
+          title: SizedBox(child: state.user != null ? Text(state.user.username) : Text('')),
+          leading: Icon(Icons.store),
+          actions: [
+            Padding(
+              padding: EdgeInsets.only(right: 12.0),
+              child: state.user != null ? IconButton(icon: Icon(Icons.exit_to_app), onPressed: () => print('pressed')) : Text('')
+            )
+          ]
+        );
+      }
+    ),
+  );
+
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar,
+      body: Container(
+        child: Text("Products Page")
+      ),
     );
   }
 }
